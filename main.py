e8be03be16c9a68031540dd7c1c3e83e4fb139bf
Manuel Emad <143853197+Exploited7@users.noreply.github.com>
2024-05-13 14:29:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-import tls_client
+import httpx
 import requests
 from colorama import Fore, init
 from concurrent.futures import ThreadPoolExecutor
-
+from json import dumps
 init()
 
 
-session = tls_client.Session(
 
-    client_identifier="chrome112",
-
-    random_tls_extension_order=True
-
-)
-
-proxy = "" # FILL THIS  USER:PASS@IP:PORT ( WE ARE USING PROXIES TO AVOID RATE LIMIT ALSO FOR CAPTCHA )
-api_key = "" # FCAP API KEY | you can get it from .gg/fcap
+proxy = "ur rotating shit proxies : user:pass@ip:port"
+api_key = "fcap key"
 
 #////////////////////////////////////////////////////////////////////////////#
 
 
+session = httpx.Client(proxies=f'http://{proxy}')
 
 print(f'''
 {Fore.LIGHTCYAN_EX}
@@ -69,20 +63,52 @@ def solveCaptcha(rqdata):
                 continue
            
             capkey = data["task"]["captcha_key"]
+            if capkey == None:
+                return solveCaptcha(rqdata)
             return capkey
     except Exception as e:
+        print(e)
         print(F'{Fore.RESET}[{Fore.LIGHTRED_EX}FAILED{Fore.RESET}] {Fore.LIGHTBLUE_EX} Failed to solve ')
         return None
+
+def getCookies():
+
+        headers = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Connection": "keep-alive",
+            "Host": "discord.com",
+            "sec-ch-ua": '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Windows"',
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Upgrade-Insecure-Requests": "1",
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+        }
+
+        resp2 = session.get('https://discord.com/register',headers=headers)
+        coo = resp2.cookies
+        cookies = {
+                        '__dcfduid': coo.get('__dcfduid'),
+                        '__sdcfduid': coo.get('__sdcfduid'),
+                        '__cfruid': coo.get('__cfruid'),
+                        'locale': 'en-US',
+        }
+        return cookies
 
 def sendFriend(token, user):
     try:
 
 
-        
+        cookies = getCookies()
         headers = {
                 'authority': 'discord.com',                
                 'Authorization': token,
-                'Cookie': '__dcfduid=f79a8b70f86511eea3ac5543e51156ec; __sdcfduid=f79a8b71f86511eea3ac5543e51156ec4a8a17ce9dab680fdf43763805212efc87fb1ce0dc8f1f297dc4c9b42d699765; _ga=GA1.1.1933564394.1712943226; OptanonConsent=isIABGlobal=false&datestamp=Fri+Apr+12+2024+19%3A33%3A47+GMT%2B0200+(Eastern+European+Standard+Time)&version=6.33.0&hosts=; _ga_YL03HBJY7E=GS1.1.1712943225.1.0.1712943232.0.0.0; __cfruid=9d8247042f51c8e3f87ad23406df0c5188426e70-1715019047; _cfuvid=.cEUQRcCnqdisFzvLoXINyVieT7jpvdCQjSibBbxaNc-1715019047584-0.0.1.1-604800000; cf_clearance=atXQAnjsUsc.bL9PHSmFkHGQnsYr8MSFuqHl36cwnVY-1715019201-1.0.1.1-DWJsXrcVBznvxDETLtKVnqDKMQ7n5X4BNxrrSn8VemLYDfKiPCpMCnbHOncxMR6wDhHSfLMFphGGU41_GL2pKw; locale=en-US; _ga_5CWMJQ1S0X=GS1.1.1715026789.3.1.1715026920.0.0.0',
+                "Cookie":  '; '.join([f"{k}={v}" for k, v in cookies.items() if v is not None]),
                 'Origin': 'https://discord.com',
                 'Priority': 'u=1, i',
                 'Referer': 'https://discord.com/channels/@me',
@@ -93,7 +119,6 @@ def sendFriend(token, user):
                 'Sec-Fetch-Mode': 'cors',
                 'Sec-Fetch-Site': 'same-origin',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
-                'X-Context-Properties': 'eyJsb2NhdGlvbiI6IkFkZCBGcmllbmQifQ==',
                 'X-Debug-Options': 'bugReporterEnabled',
                 'X-Discord-Locale': 'en-US',
                 'X-Discord-Timezone': 'Africa/Cairo',
@@ -107,14 +132,40 @@ def sendFriend(token, user):
             Sget = session.get(f'https://discord.com/api/v9/users/{userID}/profile?with_mutual_guilds=true&with_mutual_friends=true&with_mutual_friends_count=false',headers=headers)
             
             username = Sget.json()['user']['username']
-            
-            response = session.post("https://discord.com/api/v9/users/@me/relationships",headers=headers,json={
+            data = {
                 'discriminator': None,
                 'username': user
-            })
+            }
+            headers = {
+                    'Accept': '*/*',
+                    'Accept-Encoding': 'gzip, deflate, br, zstd',
+                    'Accept-Language': 'en-US,en;q=0.9',
+                    'Authorization': token,
+                    'Connection': 'keep-alive',
+                    "Content-Length": str(len(dumps(data))),
+                    'Content-Type': 'application/json',
+                    "Cookie":  '; '.join([f"{k}={v}" for k, v in cookies.items() if v is not None]),
+                    'Host': 'discord.com',
+                    'Origin': 'https://discord.com',
+                    'Referer': 'https://discord.com/channels/@me',
+                    'sec-ch-ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+                    'sec-ch-ua-mobile': '?0',
+                    'sec-ch-ua-platform': '"Windows"',
+                    'Sec-Fetch-Dest': 'empty',
+                    'Sec-Fetch-Mode': 'cors',
+                    'Sec-Fetch-Site': 'same-origin',
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+                    'X-Context-Properties': 'eyJsb2NhdGlvbiI6IkFkZCBGcmllbmQifQ==',
+                    'X-Debug-Options': 'bugReporterEnabled',
+                    'X-Discord-Locale': 'en-GB',
+                    'X-Discord-Timezone': 'Africa/Cairo',
+                    'X-Super-Properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEyNC4wLjAuMCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTI0LjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiJodHRwczovL2Rpc2NvcmQuY29tL2NoYW5uZWxzL0BtZSIsInJlZmVycmluZ19kb21haW4iOiJkaXNjb3JkLmNvbSIsInJlZmVycmVyX2N1cnJlbnQiOiIiLCJyZWZlcnJpbmdfZG9tYWluX2N1cnJlbnQiOiIiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfYnVpbGRfbnVtYmVyIjoyOTE5NjMsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGwsImRlc2lnbl9pZCI6MH0='
+            }
+            response = session.post("https://discord.com/api/v9/users/@me/relationships",headers=headers,json=data)
             if response.status_code == 200 or response.status_code == 204 :
                     print(F"{Fore.RESET}[{Fore.LIGHTGREEN_EX}SUCCESS{Fore.RESET}] {Fore.LIGHTBLUE_EX} Added , {Fore.LIGHTBLACK_EX} Username = {username} ")
             elif response.status_code == 400:
+                
                 print(f"{Fore.RESET}[{Fore.LIGHTRED_EX}CAPTCHA{Fore.RESET}] {Fore.LIGHTBLUE_EX} Captcha Required , {Fore.LIGHTBLACK_EX} Username = {username}")
                 capkey = solveCaptcha(response.json()['captcha_rqdata'])
                 headers['X-Captcha-Key'] = capkey
@@ -125,6 +176,7 @@ def sendFriend(token, user):
                 })
                 if response2.status_code == 204 or response2.status_code == 200 :
                     print(F"{Fore.RESET}[{Fore.LIGHTGREEN_EX}SUCCESS{Fore.RESET}] {Fore.LIGHTBLUE_EX} Added , {Fore.LIGHTBLACK_EX} Username = {username} ")
+                
                 else:
                     print(F'{Fore.RESET}[{Fore.LIGHTRED_EX}FAILED{Fore.RESET}] {Fore.LIGHTBLUE_EX} Adding failed after solving captcha, with status code : ',{response2.status_code})
             elif response.status_code == 429:
@@ -136,7 +188,7 @@ def sendFriend(token, user):
         else:
             print()
     except Exception as e :
-        print("hi")
+        print(e)
 
 def process_tokens(tokens_file):
     with open(tokens_file, 'r') as file:
